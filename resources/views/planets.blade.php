f82598fb6a310de7a66f6b8f526339cdd63bb1f9
Arsema <arsema.solomonbarhane>
2026-03-09 14:50:49 +0100

    Controller maken met Artisan

--- resources/views/planets.blade.php
+++ resources/views/planets.blade.php
@@ -4,17 +4,14 @@
     <title>Planets</title>
 </head>
 <body>
-
-    <h1>Planets</h1>
-
-    <ul>
-        @foreach ($planeten as $planet)
-            <li>
-                <strong>{{ $planet['name'] }}</strong><br>
-                {{ $planet['description'] }}
-            </li>
-        @endforeach
-    </ul>
+    <h1>Alle planeten</h1>
+    
+    @foreach($planeten as $key => $planet)
+        <h2>
+            <a href="/planets/{{ $key }}">{{ $planet['name'] }}</a>
+        </h2>
+        <p>{{ $planet['description'] }}</p>
+    @endforeach
 
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlanetController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
 
 Route::get('/planeten', function () { 
     return [ 
@@ -18,38 +20,8 @@ Route::get('/planeten', function () {
         "Venus" 
     ]; 
 });
-Route::get('/planets', function () {
 
-    $planets = [
-        [
-            'name' => 'Mars',
-            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
-        ],
-        [
-            'name' => 'Venus',
-            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
-        ],
-        [
-            'name' => 'Earth',
-            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
-        ],
-        [
-            'name' => 'Jupiter',
-            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
-        ],
-    ];
-    
-    $collection = collect($planets);
 
-    if (request()->has('planeet')) {
-        $zoekterm = strtolower(request('planeet'));
-
-        $collection = $collection->filter(function ($planet) use ($zoekterm) {
-            return strtolower($planet['name']) === $zoekterm;
-        });
-    }
-
-    return view('planets', ['planeten' => $collection->all()]);
-});
-
+Route::get('/planets', [PlanetController::class, 'index']);
+Route::get('/planets/{planet}', [PlanetController::class, 'show']);
 ?>
