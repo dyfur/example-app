63ac029fc89dfef96d41b89a0f7896514e13dcdb
Arsema <arsema.solomonbarhane>
2026-05-17 22:21:58 +0200

    eerste query

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,3 @@ Route::get('/planeten', function () {
 
 Route::get('/planets', [PlanetController::class, 'index'])->name('planets.index');
 Route::get('/planets/{planet}', [PlanetController::class, 'show'])->name('planets.show');
-?>
